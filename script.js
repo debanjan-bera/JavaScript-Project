25b99544442c095de669480e25bf65589d96c3ee
debanjan-bera <debanjan.bera25discord@gmail.com>
2024-06-02 18:20:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/*let a = 12;
+let b="12";
+console.log(a==b);
+let str ="Debanjan bera";
+console.log(str.trim());
+// 
+let conCate =str.trim();
+console.log(str.length);
+let char = str.charAt(2);
+console.log(char);
+let proPerty=str[4];
+console.log(proPerty);
+console.log(str.replace("bera","Coders"));
+*/
